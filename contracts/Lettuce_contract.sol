4e2eef1a368b1be56d00d1e74d09ffde34a4f294
DauletMaker <dauletmu@buffalo.edu>
2026-04-06 22:40:46 -0400

    Added the smart contract for lettuce

---
+++ contracts/Lettuce_contract.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IERC20 {
+    function transferFrom(address from, address to, uint amount) external returns (bool);
+}
+
+contract LettuceMarket {
+
+    IERC20 public yodaToken;
+
+    uint public nextId;
+
+    constructor(address _yodaToken) {
+        yodaToken = IERC20(_yodaToken);
+    }
+
+    struct Lettuce {
+        uint id;
+        address seller;
+        uint pricePerUnit;
+        uint quantity;
+        string category;
+        string quality;
+    }
+
+    mapping(uint => Lettuce) public lettuces;
+
+    event LettuceListed(uint id, address seller, uint price, uint quantity);
+    event LettucePurchased(uint id, address buyer, uint amount);
+
+    function listLettuce(
+        uint pricePerUnit,
+        uint quantity,
+        string memory category,
+        string memory quality
+
+    ) public {
+
+        require(pricePerUnit > 0, "Price must be > 0");
+        require(quantity > 0, "Quantity must be > 0");
+
+        lettuces[nextId] = Lettuce({
+            id: nextId,
+            seller: msg.sender,
+            pricePerUnit: pricePerUnit,
+            quantity: quantity,
+            category: category,
+            quality: quality
+
+        });
+
+        emit LettuceListed(nextId, msg.sender, pricePerUnit, quantity);
+
+        nextId++;
+    }
+
+    function buyLettuce(uint id, uint amount) public {
+        Lettuce storage item = lettuces[id];
+
+        require(item.quantity >= amount, "Not enough lettuce");
+
+        uint finalAmount = amount;
+
+        uint totalPrice = item.pricePerUnit * amount;
+
+        if (amount >= 10) {
+            totalPrice = (totalPrice * 80) / 100;
+        }
+
+        require(
+            yodaToken.transferFrom(msg.sender, item.seller, totalPrice),
+            "Payment failed"
+        );
+
+        require(item.quantity >= finalAmount, "Not enough stock for deal");
+        item.quantity -= finalAmount;
+
+        emit LettucePurchased(id, msg.sender, finalAmount);
+    }
+
+    function getLettuce(uint id) public view returns (Lettuce memory) {
+        return lettuces[id];
+    }
+}
